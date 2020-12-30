a7558eaa80c7c6695ac778eb12b62e4584c375c1
surajmt8848 <suraj8848mt@gmail.com>
2020-12-30 14:42:57 +0545

    create custom user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, 
+    PermissionsMixin, 
+    BaseUserManager
+    )
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError("User must have an email")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, name, email, password=None):
+        user = self.create_user(name, email, password=password)
+        user.is_active = True
+        user.is_staff = True
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name= models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+    
+    def get_short_name(self):
+        return self.name
+    
+    def __str__(self):
+        return self.email
