78224d7ae128fae69eccb52b06951702c1ad59e5
surajmt8848 <suraj8848mt@gmail.com>
2020-12-30 14:51:11 +0545

    register djoser url

--- dj_auth/urls.py
+++ dj_auth/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls'))
+    path('api/', include('accounts.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt'))
 ]
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name="index.html"))]
