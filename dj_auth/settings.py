52917efd421cffe7fa6d0a9caaa909181c88a35e
surajmt8848 <suraj8848mt@gmail.com>
2020-12-30 14:26:10 +0545

    registered app

--- dj_auth/settings.py
+++ dj_auth/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # extensions
+    'rest_framework',
+    'djoser',
+
+    # My App
+    'accounts'
 ]
 
 MIDDLEWARE = [
