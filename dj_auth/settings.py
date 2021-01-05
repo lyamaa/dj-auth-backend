e5fd117403d4b033a0367e8a50fdd5d3b0487485
surajmt8848 <suraj8848mt@gmail.com>
2021-01-05 20:39:40 +0545

    change google key

--- dj_auth/settings.py
+++ dj_auth/settings.py
@@ -202,8 +202,8 @@ DJOSER = {
     }
 }
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '824689539090-53938ubhedaf6gq2rpu8glp7j0bt6c7h.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'SCg_KOWZPN5AEE3UfsUKa1zo'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '824689539090-3ss800md5vii55nf6352ch1nvu4odd6l.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'PXG-64Pd7b3tm6s8hGX6xfpk'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['https://www.googleapis.com/auth/userinfo.email',
                                    'https://www.googleapis.com/auth/userinfo.profile', 'openid']
 SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ['first_name', 'last_name']
